4cc80715b7222fda3f887a716a5aac832c4c3aef
Mayrabrand <simkondalukas@gmail.com>
2026-09-11 08:07:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ async function sendStartupMessage(socket, s) {
         const expLine        = await expiryLine().catch(() => "✅ Active");
 
         const msg = [
-    `╭━━━〔 ⚡ *${botName}* 〕━━━╮`,
+    `╭━━━〔  *${botName}* 〕━━━╮`,
     `┃`,
     `┃ 🟢 Status  : ✅ ONLINE`,
     `┃ 📊 Plugins : ${totalCommands}`,
