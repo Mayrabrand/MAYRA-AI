7d03894fb879d922e661fab8abe4bf613593d898
Mayrabrand <simkondalukas@gmail.com>
2026-09-11 10:41:35 +0300

    Update default bot name in contextInfo.js

--- mayra/contextInfo.js
+++ mayra/contextInfo.js
@@ -1,7 +1,7 @@
 const { getSetting } = require("./database/settings");
 
 async function getContextInfo(mentionedJid = []) {
-    const botName = await getSetting("BOT_NAME") || "𝐔𝐋𝐓𝐑𝐀 𝐆𝐔𝐑𝐔";
+    const botName = await getSetting("BOT_NAME") || "𝐌𝐀𝐘𝐑𝐀-𝐀𝐈";
     const channelJid = await getSetting("NEWSLETTER_JID") || "120363406649804510@newsletter";
     return {
         mentionedJid,
